8f303864ffe101594593e9c02b51d59c03929d64
AmIWatchU <user106512@gmail.com>
2021-05-28 18:47:10 +0300

    Add project files.

---
+++ Ex2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+//        2. Максимальне з трьох чисел
+//          Завдання: Напишіть програму, яка знаходить максимальне з трьох дробових чисел,
+//          що вводяться з клавіатури, використовуючи перевантаження методу для пошуку
+//          максимального з двох значень.
+
+        static double Max(double a,double b,double c)
+        {
+            return Max(a, Max(b, c));
+        }
+        static double Max(double a, double b)
+        {
+            return Math.Max(a, b);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Enter number for x value:");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number for y value:");
+            double y = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number for z value:");
+            Console.WriteLine("P.S. Enter 0 if you want find max of 2 numbers");
+            double z = double.Parse(Console.ReadLine());
+
+            if (z == 0)
+                Console.WriteLine("Максимальне число: " + Max(x, y));
+            else
+                Console.WriteLine("Максимальне число: " + Max(x,y,z));
+        }
+    }
+}
---
+++ pract7_OOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace pract7_OOP
+{
+    class Program
+    {
+        //1. Сигнатура методу
+        //  Завдання: Визначіть сигнатуру приведеного методу:
+        //  int Summa(int a, int b = 0, int c = 0)
+        //           {
+        //                return a + b + c;
+        //            }
+        public static int Summa(int a, int b = 0, int c = 0)
+        {
+            return a + b + c;
+        }
+        static void Main(string[] args)
+        {
+            //Summa(int, int, int) - сигнатура метода 
+            Console.WriteLine(Summa(4, 7, 2));
+        }
+    }
+}
